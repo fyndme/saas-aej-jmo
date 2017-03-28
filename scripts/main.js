422aebd2b2f8a2106f12a84cea52aea608e2dc6c
bot-maker <none>
2017-03-28 21:25:49 +0000

    Saved with alana.cloud

--- scripts/main.js
+++ scripts/main.js
@@ -8,15 +8,14 @@ addGreeting((user, response) => {
     response.sendText("I'll be showing you today's space photo!")
 })
 
-newScript()
-    .dialog((session, response, stop) => {
-        request({
-            uri: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY',
-            method: 'GET',
-            json: true,
-        })
-            .then((nasa) => {
-                response.sendText(nasa.title);
-                response.sendImage(nasa.url);
-            })
+newScript().dialog((session, response, stop) => {
+    // yes DEMO_KEY works, just lay off spamming nasa :)
+    request({
+        uri: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY',
+        method: 'GET',
+        json: true,
+    }).then((nasa) => {
+        response.sendText(nasa.title);
+        response.sendImage(nasa.url);
     })
+})
