0f8d8bb839b8564bee83081819ee96199009dc27
bot-maker <none>
2017-03-28 21:01:57 +0000

    Saved with alana.cloud

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,12 @@
 // getScript
 // addGreeting
 // request
+
+addGreeting((user, response) => {
+    response.sendText("I'll be showing you today's space photo!")
+})
+
+newScript()
+    .dialog((session, response, stop) => {
+        response.sendImage('http://apod.nasa.gov/apod/image/1703/KingOfWings_Pinkston_960.jpg');
+    })
