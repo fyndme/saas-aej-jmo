cabeabbab5903743ee1a32d3b2cbaec4aeacc6cc
bot-maker <none>
2017-03-28 21:07:33 +0000

    Saved with alana.cloud

--- scripts/main.js
+++ scripts/main.js
@@ -10,5 +10,13 @@ addGreeting((user, response) => {
 
 newScript()
     .dialog((session, response, stop) => {
+        request({
+            uri: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY',
+            method: 'GET',
+            json: true,
+        })
+            .then((response) => {
+                response.sendText(response.title)
+            })
         response.sendImage('http://apod.nasa.gov/apod/image/1703/KingOfWings_Pinkston_960.jpg');
     })
